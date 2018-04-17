7083d1d7a68feda681abce9ad234306994f80537
Treasure <treasure.rose@gmail.com>
2018-04-17 08:54:44 -0700

    code review changes

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const cards = [
   {
-    'front': 'Front of card 1',
-    'back': 'Back of card 1',
-    'flipped': false,
+    front: 'The "First Computer Programmer"',
+    back: 'Ada Lovelace',
+    flipped: false,
   },
   {
-    'front': 'Front of card 2',
-    'back': 'Back of card 2',
-    'flipped': false,
+    front: 'Invented the "Clarke Calculator"',
+    back: 'Edith Clarke',
+    flipped: false,
 
   },
   {
-    'front': 'Front of card 3',
-    'back': 'Back of card 3',
-    'flipped': false,
+    front: 'Famous World War II Enigma code breaker',
+    back: 'Alan Turing',
+    flipped: false,
   },
-] 
+  {
+    front: 'Created satellite orbit analyzation software for NASA',
+    back: 'Dr. Evelyn Boyd Granville',
+    flipped: false,
+  },
+]; 
 
 new Vue({
   el: '#flashcard-app',
@@ -30,13 +35,14 @@ new Vue({
       card.flipped = !card.flipped;
     },
     addNew: function(){
+      //!this.newFront || !this.newBack also works
       if(!this.newFront.length || !this.newBack.length){
         this.error = true;
       } else {
         this.cards.push({
-          'front': this.newFront,
-          'back': this.newBack,
-          'flipped': false
+          front: this.newFront,
+          back: this.newBack,
+          flipped: false
         });
         this.newFront = '';
         this.newBack = '';
